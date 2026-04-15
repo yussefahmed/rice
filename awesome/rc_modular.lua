6ac9fa2b320bfed3e8fabeb76c5e90f761a68d8d
youssef ahmed <youssef@youssef.home>
2026-04-15 02:51:01 +0200

    configuration: extract autostart commands to module

--- awesome/rc_modular.lua
+++ awesome/rc_modular.lua
@@ -559,15 +559,4 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- CUSTOM CONFIGS
 beautiful.useless_gap = 0
 
--- Autostart Applications
--- We chain commands with '&&' or ';' to ensure order: Kill -> Wait -> Start
-
--- 1. Compositor (Picom)
-awful.spawn.with_shell("killall -q picom; sleep 0.5; picom -b")
-
--- 2. Status Bar (Polybar)
-awful.spawn.with_shell("killall -q polybar; sleep 0.5; polybar")
-
--- 3. Wallpaper (Nitrogen)
--- We wait a bit longer to ensure the desktop is ready
-awful.spawn.with_shell("sleep 1; nitrogen --restore")
+require("configuration.autostart")
